01aab985a07918e4e8b2f6f8b5f967469652efa2
aricept <aricept@users.noreply.github.com>
2023-02-06 15:01:33 -0600

    Initial commit
    Basic bot structure with cogs. Minesweeper command.

---
+++ cogs/controlcogs.py
@@ -0,0 +1,78 @@
+from discord.ext import commands
+import traceback
+
+# Commands to load and unload other cogs/extensions
+# This way we can work on just a single module and then reload it without impacting the rest of the bot
+
+class ControlCogsCog(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    # Loads a cog/extension - `cog` parameter must be a dot-separated path, e.g. cogs.gifs
+    @commands.command(name='load', hidden=True)
+    @commands.is_owner()
+    async def ext_load(self, ctx, *, cog: str):
+        """Command which Loads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        try:
+            await self.bot.load_extension(cog)
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+            traceback.print_exc()
+        else:
+            await ctx.send(f'`{cog}` loaded successfully.')
+            print(f"Loaded cog {cog}")
+
+    # Unloads cog/extension
+    @commands.command(name='unload', hidden=True)
+    @commands.is_owner()
+    async def ext_unload(self, ctx, *, cog: str):
+        """Command which Unloads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        try:
+            await self.bot.unload_extension(cog)
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        else:
+            await ctx.send('**`SUCCESS`**')
+
+    # Reloads a cog/extension
+    @commands.command(name='reload', hidden=True)
+    @commands.is_owner()
+    async def ext_reload(self, ctx, *, cog: str):
+        """Command which Reloads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        try:
+            await self.bot.unload_extension(cog)
+            await self.bot.load_extension(cog)
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        else:
+            await ctx.send('**`SUCCESS`**')
+
+    # Sync any application commands / slash commands
+    @commands.command()
+    @commands.is_owner()
+    async def sync(self, ctx):
+        await self.bot.tree.sync()
+        return await ctx.reply("Commands synced")
+
+    # Gets list of current loaded extensions
+    @commands.command(name='extensions', hidden=True)
+    @commands.is_owner()
+    async def ext_get(self, ctx):
+        cogs_list = "**Current Loaded Extensions**\n"
+        try:
+            for cog in self.bot.extensions:
+                cogs_list = cogs_list + f"{cog}\n"
+        except Exception as e:
+            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+        else:
+            await ctx.send(cogs_list)
+
+async def setup(bot):
+    await bot.add_cog(ControlCogsCog(bot))
---
+++ cogs/minesweeper.py
@@ -0,0 +1,92 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import random
+import math
+
+class Minesweeper(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        super().__init__()
+
+    @commands.command(name="minesweeper", aliases=["ms"])
+    async def new_game(self, ctx, width: commands.Range[int, 3, 15]=9, height: commands.Range[int, 3, 15]=9, bombs: int=12):
+        """
+        Start a new game of minesweeper.
+
+        Optionally, a game grid width, height, and number of bombs may be passed in.
+
+        Parameters
+        ----------
+        width : int
+            The width of the game grid (3-15) (optional)
+        height : int
+            The height of the game grid (3-15) (optional)
+        bombs : int
+            The number of bombs to include on the game grid (1- 1/3 the grid size) (optional)
+        """
+        max_bombs = math.floor((width * height) / 3)
+        if bombs < 1:
+            raise commands.RangeError(bombs, 1, (width * height) / 3)
+        if bombs > max_bombs:
+            bombs = max_bombs
+            await ctx.send(f"Max bombs allowed for {width}x{height} board is {max_bombs}, using {max_bombs}.")
+        board = Board(width, height, bombs)
+        await ctx.send(f"Grid: {width}x{height} | Bombs: {bombs}\n{board.get_board()}")
+        
+    @new_game.error
+    async def new_game_error(self, ctx, error):
+        if isinstance(error, commands.RangeError):
+            return await ctx.send("Board width and height must be between 3 and 15, and number of bombs must be at least one, up to 1/3 of the board size. Please try again.")
+        elif isinstance(error, commands.BadArgument):
+            return await ctx.send("Please enter a valid number for width, height, and number of bombs.")
+        else:
+            await ctx.send(f"`ERROR`\n{error}")
+            raise error
+
+class Board:
+    def __init__(self, width: int, height: int, bombs: int):
+        self._width = width
+        self._height = height
+        self._bombs = bombs
+        self._icons = {
+            0: "||:zero:||",
+            1: "||:one:||",
+            2: "||:two:||",
+            3: "||:three:||",
+            4: "||:four:||",
+            5: "||:five:||",
+            6: "||:six:||",
+            7: "||:seven:||",
+            8: "||:eight:||",
+            "bomb": "||:boom:||"
+        }
+        self.board = [[0 for x in range(self._width)] for y in range(self._height)]
+        for i in range(self._bombs):
+            self._place_bomb()        
+
+    def _place_bomb(self):
+        column = random.randint(0, self._width - 1)
+        row = random.randint(0, self._height - 1)
+        if self.board[row][column] == "bomb":
+            self._place_bomb()
+        else:
+            self.board[row][column] = "bomb"
+            for x in [row - 1, row, row + 1]:
+                if x > self._height - 1 or x < 0:
+                    continue
+                for y in [column - 1, column, column + 1]:
+                    if y > self._width - 1 or y < 0 or self.board[x][y] == "bomb":
+                        continue
+                    else:
+                        self.board[x][y] += 1
+
+    def get_board(self):
+        rows = []
+        for row in self.board:
+            formatted = [self._icons[cell] for cell in row]
+            rows.append("".join(formatted))
+        return "\n".join(rows)
+
+async def setup(bot):
+    await bot.add_cog(Minesweeper(bot))
